685ebc6ecbd11fb5ba86fc3e5248eff0138bacfa
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-05-25 19:12:26 -0500

    solving the functions and pass the test

--- find-findindex.js
+++ find-findindex.js
@@ -11,8 +11,9 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find((element) => element.username === username);
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +28,9 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  const index = usersArray.findIndex(
+    (element) => element.username === username
+  );
+  if (index >= 0) return usersArray.splice(index, 1)[0];
+}
